a362e8956c8f54031cbeb5a1ffbe70bc12c1cba3
Vi Tran <contact.vitran@gmail.com>
2022-02-09 22:28:11 +1100

    Add ingredients list updated

--- src/components/AddRecipe.js
+++ src/components/AddRecipe.js
@@ -1,6 +1,20 @@
 import cookbooks from "../cookbook-data";
+import { useState } from "react";
 
 export default function AddRecipe() {
+  const [ingredients, setIngredients] = useState(["test"]);
+
+  const addNewIngredient = () => {
+    setIngredients([...ingredients, ""]);
+  }
+
+  const handleIngredientChange = (event) => {
+    const index = event.target.attributes.datakey.value;
+    const newArray = [...ingredients];
+    newArray[index] = event.target.value;
+    setIngredients(newArray);
+  };
+
   return (
     <div>
       <form>
@@ -8,7 +22,7 @@ export default function AddRecipe() {
           <h2>Step 1: Recipe Title and Description</h2>
           <label htmlFor="title">Title: </label>
           <br></br>
-          <input type="text" name="title" />
+          <input type="text" name="title" required />
           <br></br>
           <label htmlFor="description">Description: </label>
           <br></br>
@@ -20,14 +34,23 @@ export default function AddRecipe() {
             placeholder="Enter a short description about your recipe."
           ></textarea>
           <br></br>
-          <label htmlFor="cookbooks">Select cookbook to add recipe: </label>
+          <label htmlFor="cookbooks">
+            Add this recipe to my cookbooks (optional):{" "}
+          </label>
           <br></br>
-          <select name="cookbooks" id="cookbooks">
-            <option>-None-</option>
-            {cookbooks.map((book) => {
-              return <option value={book.title}>{book.title}</option>;
-            })}
-          </select>
+          {cookbooks.map((book) => {
+            return (
+              <div>
+                <input
+                  type="checkbox"
+                  id={book.id}
+                  name={book.title}
+                  value={book.title}
+                />
+                <label for={book.title}>{book.title}</label>
+              </div>
+            );
+          })}
         </div>
         <div>
           <h2>Step 2: Recipe information</h2>
@@ -38,7 +61,7 @@ export default function AddRecipe() {
           <input type="text" name="cook time" />
           <br></br>
           <label htmlFor="serves">Serves: </label>
-          <input type="text" name="serves" />
+          <input type="text" name="serves" required />
           <br></br>
           <label htmlFor="makes">Makes: </label>
           <input type="text" name="makes" />
@@ -47,18 +70,27 @@ export default function AddRecipe() {
           <h2>Step 3: Recipe Ingredients, Method and Tips</h2>
           <div>
             <h3>Ingredients</h3>
-            <input type="checkbox" id="single" name="single" value="Single" />
-            <label for="single"> Single list (e.g. cake only)</label>
-            <br></br>
-            <input type="checkbox" id="multi" name="multi" value="Multi" />
-            <label for="multi">
-              Multi-part list (e.g. ingredients for cake, ingredients for icing)
-            </label>
+
             <br></br>
             <div>
-              <input type="text" name="ingredient" />Enter one ingredient at a time.
+              {ingredients.map((item, index) => {
+                return (
+                  <div>
+                  <input
+                    key={index}
+                    type="text"
+                    name={"ingredient[" + index + "]"}
+                    value={item}
+                    dataKey={index}
+                    required
+                    onChange={handleIngredientChange}
+                  /><br></br>
+                  </div>
+                );
+              })}
+              Enter one ingredient at a time.
             </div>
-            <button type="button">+ Add another ingredient</button>
+            <button type="button" onClick={addNewIngredient}>+ Add another ingredient</button>
           </div>
         </div>
         <br></br>
